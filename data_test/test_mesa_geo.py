5ca46ce93b44cfe1920a9052c2f831688a230505
Christos Perchanidis <pmchrist@protonmail.com>
2023-01-17 00:13:12 +0100

    Added Amsterdam

---
+++ data_test/test_mesa_geo.py
@@ -0,0 +1,54 @@
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import mesa
+import mesa_geo as mg
+import json
+
+
+
+f = open('Amsterdam_map_from_gemente.geojson')
+geojson_states = json.load(f)
+
+
+
+
+
+
+class State(mg.GeoAgent):
+    def __init__(self, unique_id, model, geometry, crs):
+        super().__init__(unique_id, model, geometry, crs)
+
+
+class GeoModel(mesa.Model):
+    def __init__(self):
+        self.space = mg.GeoSpace()
+
+        ac = mg.AgentCreator(agent_class=State, model=self)
+        agents = ac.from_GeoJSON(GeoJSON=geojson_states, unique_id="Buurt")     # Set unique Id to one from dataset
+        self.space.add_agents(agents)
+
+
+
+
+
+    
+
+m = GeoModel()
+
+# Showing Params of Agent
+agent = m.space.agents[0]
+print(agent.unique_id)
+agent.geometry
+agent.Oppervlakte_m2
+# Available Params
+# CBS_Buurtcode,        Buurtcode,          Buurt,          Wijkcode,               Wijk,               Gebiedcode,     Gebied,     Stadsdeelcode,  Stadsdeel,  Oppervlakte_m2
+# Neighborhood code,    Neighborhood code,  Neighborhood,   Neighborhood Big code,  Neighbourhood Big,  Area code,      Area,       District code,  District,   Surface
+
+# Neighbour Regions
+neighbors = m.space.get_neighbors(agent)
+print([a.unique_id for a in neighbors])
+# Regioins in close space
+print([a.unique_id for a in m.space.get_neighbors_within_distance(agent, 500)])
+
